4b07dad02fa95fb4643b88d58d1aa12439d0f4e3
ErickSoares95 <ericklira99@gmail.com>
2021-08-31 16:36:38 -0300

    Domínio utilizando OO

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,34 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Endereco endereco = new Endereco("Casa", "54120-250", "Recife", "PE");
+                Cliente c1 = new Cliente("erick", "11111111111", "540045", endereco);
+
+                ContaCorrente conta = new ContaCorrente(c1, 100);
+                Console.WriteLine("Conta: " + conta.Situacao + "-" + conta.NumeroConta + conta.DigitoVerificador);
+                string senha = "acd12378";
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta: " + conta.Situacao + "-" + conta.NumeroConta + conta.DigitoVerificador);
+
+                conta.Sacar(20, senha);
+
+                Console.WriteLine("Saldo: " + conta.Saldo);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ AgenciaBancaria.Dominio/Validacoes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public static class Validacoes
+    {
+        public static string ValidarStringVazia(this string texto)
+        {
+            return string.IsNullOrWhiteSpace(texto) ?
+                            throw new Exception("Propiedade deve estar preenchida.") : texto;
+        }
+    }
+}
